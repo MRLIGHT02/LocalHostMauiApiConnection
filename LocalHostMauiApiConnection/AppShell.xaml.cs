93361d26e1d8be28b10de58adb610605d22ddf1f
MRLIGHT02 <saifali42020@gmail.com>
2025-10-04 15:33:33 +0530

    Add project files.

---
+++ LocalHostMauiApiConnection/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LocalHostMauiApiConnection
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
